24123c607f3e9238a35d8de039da23385c7c2682
FadiBunni <fadibunni_guitar@hotmail.com>
2016-09-05 01:17:33 +0200

    nyeste

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/',express.static(__dirname + '/client'));
+
+serv.listen(process.env.PORT || 2000);
+console.log('Server started.')
+
+
+
+
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+
+});
+
+
